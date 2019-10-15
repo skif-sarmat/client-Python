98fe3971bf97256cbeece519b947566c1cc5bceb
c-dzmitry.kolb <c-dzmitry.kolb@hulu.com>
2019-10-15 17:54:38 +0300

    added version undepended setting of daemon

--- reportportal_client/service_async.py
+++ reportportal_client/service_async.py
@@ -47,11 +47,8 @@ class QueueListener(object):
         This starts up a background thread to monitor the queue for
         items to process.
         """
-        if sys.version_info.major == 2:
-            self._proccess = p = multiprocessing.Process(target=QueueListener._monitor, args=(self,))
-            p.setDaemon(True)
-        elif sys.version_info.major == 3:
-            self._proccess = p = multiprocessing.Process(target=QueueListener._monitor, args=(self,), daemon=True)
+        self._proccess = p = multiprocessing.Process(target=QueueListener._monitor, args=(self,))
+        daemon = True
         p.start()
 
     def prepare(self, record):
